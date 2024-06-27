df5109e3f5fffcf431ce3db3ceec17f6919e8a14
Rachna <rachnakum0476@gmail.com>
2024-06-27 18:06:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import Biodata from './component/Biodata';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Biodata />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Biodata.js
@@ -0,0 +1,29 @@
+// Biodata.js
+
+import React from 'react';
+import './biodata.css'; // Import your CSS file
+
+const Biodata = () => {
+  return (
+      <div className="biodata">
+    
+    <img src="/images/img55.jpg" alt="Description of the image" />
+
+    <img src="/images/img11.jpg" alt="Description of the image" />
+    {/* <img src="/images/.jpg" alt="Description of the image" /> */}
+    <img src="/images/66.jpg" alt="Description of the image" />
+    <img src="/images/img22.jpg" alt="Description of the image" />
+        {/* <img src="/images/img33.jpg" alt="Description of the image" /> */}
+
+
+  
+  </div>
+  )
+}
+
+export default Biodata
+
+
+     
+
+ 
