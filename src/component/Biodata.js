4346b0d2a7c6fec4da6eb85a1a5ea078292fbbf9
Rachna <rachnakum0476@gmail.com>
2024-07-02 12:01:06 +0530

    first commit

--- src/component/Biodata.js
+++ src/component/Biodata.js
@@ -7,13 +7,14 @@ const Biodata = () => {
   return (
       <div className="biodata">
     
-    <img src="/images/img55.jpg" alt="Description of the image" />
+    <img src="/images/img555.jpg" alt="Description of the image" />
 
     <img src="/images/img11.jpg" alt="Description of the image" />
     {/* <img src="/images/.jpg" alt="Description of the image" /> */}
     <img src="/images/66.jpg" alt="Description of the image" />
     <img src="/images/img22.jpg" alt="Description of the image" />
-        {/* <img src="/images/img33.jpg" alt="Description of the image" /> */}
+       
+        <img src="/images/img33.jpg" alt="Description of the image" />
 
 
   
